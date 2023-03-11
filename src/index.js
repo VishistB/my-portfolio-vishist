9e019342c32d13d62e17282e88f8fd5e3641853f
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-03-11 21:47:03 +0530

    glass effect improved

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Navbar from './Components/Navbar/Navbar';
+import './index.css';
 
 
 // import reportWebVitals from './reportWebVitals';
