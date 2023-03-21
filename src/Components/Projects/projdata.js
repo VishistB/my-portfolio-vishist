73f03c11de952ed7989ee7b382df9ede1e438562
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-03-22 01:12:28 +0530

    fullstop lol

--- src/Components/Projects/projdata.js
+++ src/Components/Projects/projdata.js
@@ -2,16 +2,16 @@ export const projData = [
     {
         name: "Thapar Maps",
         src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1678733417/Portfolio%20Website/Tmaps_a2m2oc.png",
-        desc: "An in progress map website which will ease navigation in the campus for university Students. Uses the mapbox API"
+        desc: "An in progress map website which will ease navigation in the campus for university Students. Uses the mapbox API>"
     },
     {
         name: "College SGPA calculator",
         src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1678733072/Portfolio%20Website/Screenshot_1656531315_py5vqf.png",
-        desc: "A semester grade point calculator for colleges"
+        desc: "A semester grade point calculator for colleges."
     },
     {
         name: "UVAH",
         src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1679422021/Portfolio%20Website/UVAH_fdb6cp.png",
-        desc: "A Website for anyone who wants to get into technology and development. It has a list of vast array of courses listed for various tech domains"
+        desc: "A Website for anyone who wants to get into technology and development. It has a list of vast array of courses listed for various tech domains."
     },
 ]
