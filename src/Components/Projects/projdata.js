83a120e1cf48918dfdde269e466e573c513dbdb4
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-03-22 01:10:45 +0530

    added proj page

---
+++ src/Components/Projects/projdata.js
@@ -0,0 +1,17 @@
+export const projData = [
+    {
+        name: "Thapar Maps",
+        src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1678733417/Portfolio%20Website/Tmaps_a2m2oc.png",
+        desc: "An in progress map website which will ease navigation in the campus for university Students"
+    },
+    {
+        name: "College SGPA calculator",
+        src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1678733072/Portfolio%20Website/Screenshot_1656531315_py5vqf.png",
+        desc: "A semester grade point calculator for colleges"
+    },
+    {
+        name: "UVAH",
+        src: "https://res.cloudinary.com/dibuy1ztk/image/upload/v1679422021/Portfolio%20Website/UVAH_fdb6cp.png",
+        desc: ""
+    },
+]
